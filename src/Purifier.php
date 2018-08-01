5d3c2f0f823cf8f240cfc522a0d5538960807fc9
Alex <alexusmai@gmail.com>
2018-08-01 11:24:18 +0300

    fix settings error

--- src/Purifier.php
+++ src/Purifier.php
@@ -19,26 +19,18 @@ class Purifier
      */
     public function __construct()
     {
-        // purifier settings
-        $this->settings = \HTMLPurifier_Config::createDefault();
-
-        // load basic settings
-        $this->settings->loadArray(config('purifier.basic'));
-
         // set cache dir
         $cacheDir = config('purifier.basic')['Cache.SerializerPath'];
 
-        if (! File::isDirectory($cacheDir)) {
+        if (!File::isDirectory($cacheDir)) {
             File::makeDirectory($cacheDir, config('purifier.basic')['Cache.SerializerPermissions']);
         }
-
     }
-
 
     /**
      * Load settings and clean
      *
-     * @param string            $text
+     * @param string $text
      * @param null|string|array $customSettings
      *
      * @return array|string
@@ -46,6 +38,34 @@ class Purifier
      */
     public function clean($text, $customSettings = null)
     {
+        //load settings
+        $this->loadSettings($customSettings);
+
+        // clean text
+        $purifier = new \HTMLPurifier($this->settings);
+
+        // if it's array
+        if (is_array($text)) {
+            return array_map(function ($item) use ($purifier) {
+                return $purifier->purify($item);
+            }, $text);
+        } else {
+            return $purifier->purify($text);
+        }
+    }
+
+    /**
+     * @param $customSettings
+     * @throws \Exception
+     */
+    protected function loadSettings($customSettings)
+    {
+        // purifier settings
+        $this->settings = \HTMLPurifier_Config::createDefault();
+
+        // load basic settings
+        $this->settings->loadArray(config('purifier.basic'));
+
         // if custom settings
         if ($customSettings) {
 
@@ -71,21 +91,5 @@ class Purifier
             // load default settings
             $this->settings->loadArray(config('purifier.default'));
         }
-
-        // clean text
-        $purifier = new \HTMLPurifier($this->settings);
-
-        // if it's array
-        if (is_array($text)) {
-
-            return array_map(function ($item) use ($purifier) {
-                return $purifier->purify($item);
-            }, $text);
-
-        } else {
-
-            return $purifier->purify($text);
-        }
-
     }
 }
